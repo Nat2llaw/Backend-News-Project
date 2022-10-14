8bdf384593eaeb0aa28c8211fa164526d71c71f5
Nathaniel <nat2llaw@gmail.com>
2022-10-14 15:46:01 +0100

    heroku

---
+++ listen.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+module.exports = app.listen
