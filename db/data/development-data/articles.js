a2190d4b67ddbf4e869a41e4506b83e26b5ff23e
Nathaniel <nat2llaw@gmail.com>
2022-11-07 19:23:38 +0000

    add image test

--- db/data/development-data/articles.js
+++ db/data/development-data/articles.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     title: "Running a Node App",
-    img_URL:
+    img_url:
       "https://images.ctfassets.net/aq13lwl6616q/7cS8gBoWulxkWNWEm0FspJ/c7eb42dd82e27279307f8b9fc9b136fa/nodejs_cover_photo_smaller_size.png",
     topic: "coding",
     author: "jessjelly",
