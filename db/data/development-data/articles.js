d2fdc359b925df2b7a8d410c0628b9e34065cab9
Nathaniel <nat2llaw@gmail.com>
2022-11-10 12:42:59 +0000

    fix pull

--- db/data/development-data/articles.js
+++ db/data/development-data/articles.js
@@ -291,7 +291,7 @@ module.exports = [
     votes: 0,
   },
   {
-    title: "What to Cook This Coming Week",
+    title: "What to Cook This Week",
     img_url: "https://themom100.com/wp-content/uploads/2019/08/one-skillet-beefy-enchilada-casserole-245.jpg",
     topic: "cooking",
     author: "tickle122",
